088e6680b012035142f2e26e5e25d9b0c1eb2760
amry20 <priswantoamry@gmail.com>
2026-08-14 09:04:38 +0700

    Set clock Add a heartbeat function

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,59 @@
 #include <Arduino.h>
+extern "C" void SystemClock_Config(void)
+{
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-// put function declarations here:
-int myFunction(int, int);
+    /** Configure the main internal regulator output voltage
+     */
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
+    /** Initializes the RCC Oscillators according to the specified parameters
+     * in the RCC_OscInitTypeDef structure.
+     */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM = 8;
+    RCC_OscInitStruct.PLL.PLLN = 336;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ = 7;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+        Error_Handler();
+    }
+
+    /** Initializes the CPU, AHB and APB buses clocks
+     */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+#define HEARTBEAT_LED PC13
+void heartbeat();
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(digitalPinToPinName(HEARTBEAT_LED), OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  heartbeat();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void heartbeat() {
+  static uint32_t lastToggleTime = 0;
+  uint32_t currentTime = millis();
+
+  if (currentTime - lastToggleTime >= 500) { // Toggle every 500 ms
+    lastToggleTime = currentTime;
+    digitalToggleFast(digitalPinToPinName(HEARTBEAT_LED)); // Use digitalToggleFast for faster toggling
+  }
 }
